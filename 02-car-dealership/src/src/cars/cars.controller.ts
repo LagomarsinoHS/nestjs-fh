cb66271bb1044841d309856fcccc34c172d7fdba
Humberto Lagomarsino G <h.lagomarsino.g@gmail.com>
2023-06-05 18:45:55 -0400

    fin seccion 3

--- 02-car-dealership/src/src/cars/cars.controller.ts
+++ 02-car-dealership/src/src/cars/cars.controller.ts
@@ -1,15 +1,40 @@
-import { Controller, Get, Param, ParseIntPipe } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  Param,
+  ParseIntPipe,
+  Post,
+  Put,
+} from '@nestjs/common';
+import { CarsService } from './cars.service';
 
 @Controller('cars')
 export class CarsController {
-  private cars = ['Peugeot', 'Ford', 'Chevrolet'];
+  constructor(private readonly carsService: CarsService) {}
   @Get()
   getCars() {
-    return this.cars;
+    return this.carsService.getCars();
   }
 
   @Get(':id')
   getById(@Param('id', ParseIntPipe) id: number) {
-    return this.cars[id] || 'Not found';
+    return this.carsService.getCarById(id);
+  }
+
+  @Post()
+  postCar(@Body() payload: any) {
+    return '';
+  }
+
+  @Put(':id')
+  putCar(@Param('id', ParseIntPipe) id: number, @Body() payload: any) {
+    return '';
+  }
+
+  @Delete(':id')
+  deleteCar(@Param('id', ParseIntPipe) id: number) {
+    return '';
   }
 }
